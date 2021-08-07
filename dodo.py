ecffcb481ddad38acd4a13aeac132fd31ec0429f
bozo.kopic <bozo@kopic.xyz>
2021-08-08 00:15:09 +0200

    doit bugfix

--- dodo.py
+++ dodo.py
@@ -1,5 +1,6 @@
 from hat.doit import common
-from src_doit import *  # NOQA
 
 DOIT_CONFIG = common.init(python_paths=['src_py'],
                           default_tasks=['build'])
+
+from src_doit import *  # NOQA
