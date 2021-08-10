afaff761e2a3e9fb06864c079726a219673c980e
bozo.kopic <bozo@kopic.xyz>
2021-08-10 22:28:35 +0200

    doit update

--- src_doit/__init__.py
+++ src_doit/__init__.py
@@ -1,8 +1,12 @@
 from pathlib import Path
-import subprocess
-import sys
 
 from hat.doit import common
+from hat.doit.py import (build_wheel,
+                         run_pytest,
+                         run_flake8)
+from hat.doit.docs import (SphinxOutputType,
+                           build_sphinx,
+                           build_pdoc)
 
 from hat import json
 
@@ -36,9 +40,9 @@ def task_build():
     """Build"""
 
     def build():
-        common.wheel_build(
+        build_wheel(
             src_dir=src_py_dir,
-            dst_dir=build_dir / 'py',
+            dst_dir=build_py_dir,
             src_paths=list(common.path_rglob(src_py_dir,
                                              blacklist={'__pycache__'})),
             name='hat-json',
@@ -53,41 +57,23 @@ def task_build():
 
 def task_check():
     """Check with flake8"""
-    return {'actions': [(_run_flake8, [src_py_dir]),
-                        (_run_flake8, [pytest_dir])]}
+    return {'actions': [(run_flake8, [src_py_dir]),
+                        (run_flake8, [pytest_dir])]}
 
 
 def task_test():
     """Test"""
-
-    def run(args):
-        subprocess.run([sys.executable, '-m', 'pytest',
-                        '-s', '-p', 'no:cacheprovider',
-                        *(args or [])],
-                       cwd=str(pytest_dir),
-                       check=True)
-
-    return {'actions': [run],
-            'pos_arg': 'args',
-            'task_dep': ['json_schema_repo']}
+    return {'actions': [lambda args: run_pytest(pytest_dir, *(args or []))],
+            'pos_arg': 'args'}
 
 
 def task_docs():
     """Docs"""
-
-    def build():
-        common.sphinx_build(common.SphinxOutputType.HTML, docs_dir,
-                            build_docs_dir)
-        subprocess.run([sys.executable, '-m', 'pdoc',
-                        '--html', '--skip-errors', '-f',
-                        '-o', str(build_docs_dir / 'py_api'),
-                        'hat.json'],
-                       stdout=subprocess.DEVNULL,
-                       stderr=subprocess.DEVNULL,
-                       check=True)
-
-    return {'actions': [build],
-            'task_dep': ['json_schema_repo']}
+    return {'actions': [(build_sphinx, [SphinxOutputType.HTML,
+                                        docs_dir,
+                                        build_docs_dir]),
+                        (build_pdoc, ['hat.json',
+                                      build_docs_dir / 'py_api'])]}
 
 
 def task_json_schema_repo():
@@ -102,8 +88,3 @@ def task_json_schema_repo():
     return {'actions': [generate],
             'file_dep': src_paths,
             'targets': [json_schema_repo_path]}
-
-
-def _run_flake8(path):
-    subprocess.run([sys.executable, '-m', 'flake8', str(path)],
-                   check=True)
