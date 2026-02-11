d1f1b061428e97e463d0e5f278ed7c155620de40
Adam Trstenjak <adam@adamtrs.com>
2026-02-11 21:49:43 +0100

    add patch tests

--- test_pytest/test_patch.py
+++ test_pytest/test_patch.py
@@ -3,6 +3,10 @@ import pytest
 from hat import json
 
 
+data = {'a': [1, 2, [[], 123], {'b': 'abc'}],
+        'c': True}
+
+
 @pytest.mark.parametrize("x, y, diff", [
     ({'a': 0},
      {'a': False},
@@ -48,6 +52,113 @@ from hat import json
 def test_diff(x, y, diff):
     result = json.diff(x, y)
     assert result == diff
+
+
+@pytest.mark.parametrize("x", [[], {}, 1, False, 'a', None])
+def test_patch_empty(x):
+    result = json.patch(x, [])
+    assert result == x
+
+
+@pytest.mark.parametrize("x, diff, y", [
+    (data,
+     [{'op': 'add', 'path': '', 'value': 42}],
+     42),
+
+    (data,
+     [{'op': 'add', 'path': '/a/3/b', 'value': 'cba'}],
+     json.set_(data, ['a', 3, 'b'], 'cba')),
+
+    (data,
+     [{'op': 'add', 'path': '/a/2/0/-', 'value': 42}],
+     json.set_(data, ['a', 2, 0, 0], 42)),
+
+    (data,
+     [{'op': 'add', 'path': '/a/2/0/0', 'value': 42}],
+     json.set_(data, ['a', 2, 0, 0], 42)),
+
+    (data,
+     [{'op': 'add', 'path': '/a/2/0', 'value': 321}],
+     json.set_(data, ['a', 2], [321, [], 123]))
+])
+def test_patch_add(x, diff, y):
+    result = json.patch(x, diff)
+    assert result == y
+
+
+@pytest.mark.parametrize("x, diff, y", [
+    (data,
+     [{'op': 'remove', 'path': ''}],
+     None),
+
+    (data,
+     [{'op': 'remove', 'path': '/a/3/b'}],
+     json.remove(data, ['a', 3, 'b'])),
+
+    (data,
+     [{'op': 'remove', 'path': '/a/2/0'}],
+     json.remove(data, ['a', 2, 0]))
+])
+def test_patch_remove(x, diff, y):
+    result = json.patch(x, diff)
+    assert result == y
+
+
+@pytest.mark.parametrize("x, diff, y", [
+    (data,
+     [{'op': 'replace', 'path': '', 'value': 42}],
+     42),
+
+    (data,
+     [{'op': 'replace', 'path': '/a/3/b', 'value': 'cba'}],
+     json.set_(data, ['a', 3, 'b'], 'cba')),
+
+    (data,
+     [{'op': 'replace', 'path': '/a/2/0', 'value': 321}],
+     json.set_(data, ['a', 2, 0], 321))
+])
+def test_patch_replace(x, diff, y):
+    result = json.patch(x, diff)
+    assert result == y
+
+
+@pytest.mark.parametrize("x, diff, y", [
+    (data,
+     [{'op': 'move', 'from': '', 'path': ''}],
+     data),
+
+    (data,
+     [{'op': 'move', 'from': '/c', 'path': '/a'}],
+     {'a': True})
+])
+def test_patch_move(x, diff, y):
+    result = json.patch(x, diff)
+    assert result == y
+
+
+@pytest.mark.parametrize("x, diff, y", [
+    (data,
+     [{'op': 'copy', 'from': '', 'path': ''}],
+     data),
+
+    (data,
+     [{'op': 'copy', 'from': '/c', 'path': '/a'}],
+     {'a': True, 'c': True})
+])
+def test_patch_copy(x, diff, y):
+    result = json.patch(x, diff)
+    assert result == y
+
+
+@pytest.mark.parametrize("x, diff", [
+    (data,
+     [{'op': 'test', 'path': '', 'value': data}]),
+
+    (data,
+     [{'op': 'test', 'path': '/a/3', 'value': {'b': 'abc'}}])
+])
+def test_patch_test(x, diff):
+    json.patch(x, diff)
 
 
 def test_diff_example():
